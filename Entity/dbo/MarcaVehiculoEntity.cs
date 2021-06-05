c2bcb00807129f4a55bda7340a6be9d97ed132a0
karvari-15 <karen@DESKTOP-PF11SPK>
2021-06-04 20:16:34 -0600

    Actualizacion

---
+++ Entity/dbo/MarcaVehiculoEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Entity
+{
+    public class MarcaVehiculoEntity: EN
+    {
+        public int? MarcaVehiculoId { get; set; }//puede ir nullo
+        public string Descripcion { get; set; }
+    }
+}
